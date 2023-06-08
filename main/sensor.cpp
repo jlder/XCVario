99a0d630d9c333c477ddbcfe6adfb0824e1505f0
jlder <jeanluc@derouineau.com>
2023-06-08 11:32:18 +0200

    WIP i2c 400k to prev 100k to help vario stability

--- main/sensor.cpp
+++ main/sensor.cpp
@@ -1824,7 +1824,7 @@ void system_startup(void *args){
 	bool selftestPassed=true;
 	int line = 1;
 	ESP_LOGI( FNAME, "Now setup I2C bus IO 21/22");
-	i2c.begin(GPIO_NUM_21, GPIO_NUM_22, 400000 );
+	i2c.begin(GPIO_NUM_21, GPIO_NUM_22, 100000 ); // TODO check optimum frequency. Reduce from 400khz to original 100khz just in case it helps solving vario stability.
 	Router::begin();
 	theWind.begin();
 
