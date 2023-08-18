498cad9df0a4e30640997f80fa56142deb72c007
jlder <jeanluc@derouineau.com>
2023-08-18 18:16:38 +0200

    correct Kgain control parameter

--- main/sensor.cpp
+++ main/sensor.cpp
@@ -781,6 +781,8 @@ float deltaGz;
 			#define fcGravDec2 ( 1.0 - fcGravDec1 )
 			AccelGravModuleFilt = fcGravDec1 * AccelGravModuleFilt + fcGravDec2 * abs(AccelGravModule - GRAVITY);
 		}
+		
+		/* TODO improve parameter to control Kgain
 		if ( TAS > 15 ) {
 			// when moving compute error between acceleration module and local gravity (GRAVITY) in relation to Nlimit
 			GravityModuleErr = Nlimit - AccelGravModuleFilt;
@@ -788,7 +790,9 @@ float deltaGz;
 		{
 			// when on ground compute error using both accel and gyro module variation
 			GravityModuleErr =  (Nlimit - AccelGravModuleFilt) + (GyroModulePrimLevel - Gyroprimlimit);
-		}
+		}*/
+		
+		GravityModuleErr = Nlimit - AccelGravModuleFilt;		
 		
 		/* TODO need to rework the concept of AGC for Kgain
 		// if GravityModuleErr positive, high confidence in accels
@@ -1407,9 +1411,9 @@ void readSensors(void *pvParameters){
 	float WingLoad = 40.0;
 	float AoA = 0.0;
 	float AoB = 0.0;
-    float CLA = 5.67; // CLA=2*PI/(1+2/AR) = 5.75 for LS6, 5.98 for Ventus 3, 5.67 for Taurus
-    float KAoB = 3.0; // 3.5 for LS6,  2.97 for Ventus 3, 3 for Taurus TBC
-    float KGx = 4.0; // 4.1 for LS6, 12 for Ventus 3, 4 for Taurus TBC
+    float CLA = 5.75; // CLA=2*PI/(1+2/AR) = 5.75 for LS6, 5.98 for Ventus 3, 5.67 for Taurus
+    float KAoB = 3.5; // 3.5 for LS6,  2.97 for Ventus 3, 3 for Taurus TBC
+    float KGx = 4.1; // 4.1 for LS6, 12 for Ventus 3, 4 for Taurus TBC
 	
 	float TASbiSquare;
 	float deltaEnergy;
