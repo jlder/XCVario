cb68347cd39accc073158dcd1e450099f40432b0
Jean-Luc Derouineau <jeanluc@derouineau.com>
2024-04-19 17:19:04 +0200

    Allow up to 100 ground bias/gravity estimates

--- main/sensor.cpp
+++ main/sensor.cpp
@@ -1387,7 +1387,7 @@ static void processIMU(void *pvParameters)
 			}	
 			
 			// Estimate gyro bias and gravity up to 10 times, except if doing Lab test then only one estimation is performed
-			if ( (BIAS_Init < 10 && !LABtest) || BIAS_Init < 1 ) {
+			if ( (BIAS_Init < 100 && !LABtest) || BIAS_Init < 1 ) {
 				// When MPU temperature is controled and temperature is locked   or   when there is no temperature control
 				if ( (HAS_MPU_TEMP_CONTROL && (MPU.getSiliconTempStatus() == MPU_T_LOCKED)) || !HAS_MPU_TEMP_CONTROL ) {
 					// count cycles when temperature is locked
